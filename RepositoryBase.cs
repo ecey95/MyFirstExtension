ce143817935f9c93a43c3a2e02df33b42ad89d55
Emre <emrecyln7@gmail.com>
2019-02-23 21:43:12 +0300

    Create RepositoryBase.cs

---
+++ RepositoryBase.cs
@@ -0,0 +1,32 @@
+﻿using MyEvernote.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.BusinessLayer
+{
+    public class RepositoryBase
+    {
+        private static DatabaseContext _db;
+        private static object _lockSync = new object(); // İstediğin ismi verebilirsin.
+
+        protected RepositoryBase() { }
+
+        public static DatabaseContext CreateContex()
+        {
+            if (_db == null)
+            {
+                lock (_lockSync)
+                {
+                    if (_db == null)
+                    {
+                        _db = new DatabaseContext();
+                    }
+                }
+            }
+            return _db;
+        }
+    }
+}
